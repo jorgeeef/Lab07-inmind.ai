21f13cd8d184eea21200c6b0b68f5ead590e949b
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-02 17:22:48 +0200

    Static files

---
+++ Lab07/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab07.Controllers;
+
+public class UserController:ControllerBase
+{
+    [HttpPost("upload-profile-image")]
+    public async Task<IActionResult> UploadProfileImage(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+        
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "profiles", fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+    
+        return Ok(new { FilePath = $"/images/profiles/{fileName}" });
+    }
+}
